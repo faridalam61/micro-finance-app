87ae35caf83eebfe99dbb7c4eac658cb63e9c60b
Farid Alam <faridalam62@gmail.com>
2025-02-10 14:21:19 +0600

    Added dashboard and login page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,27 @@
-import {Button} from './components/ui/button'
+import { Route, Routes } from "react-router";
+import DefaultLayout from "./layout/DefaultLayout";
+import Dashboard from "./pages/Dashboard";
+import { ThemeProvider } from "./components/theme-provider";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
- 
+	return (
+		<ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+			<Routes>
+				<Route path="/login" element={<LoginPage />} />
 
-  return (
-    <Button>Test</Button>
-  )
+				<Route element={<DefaultLayout />}>
+					<Route index element={<Dashboard />} />
+				</Route>
+				{/* 
+			<Route path="concerts">
+				<Route index element={<ConcertsHome />} />
+				<Route path=":city" element={<City />} />
+				<Route path="trending" element={<Trending />} />
+			</Route> */}
+			</Routes>
+		</ThemeProvider>
+	);
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/login-form.tsx
@@ -0,0 +1,70 @@
+import { Button } from "../components/ui/button";
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle,
+} from "../components/ui/card";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
+import { cn } from "../lib/utils";
+
+export function LoginForm({
+	className,
+	...props
+}: React.ComponentPropsWithoutRef<"div">) {
+	const currentYear = new Date().getFullYear();
+	return (
+		<div className={cn("flex flex-col gap-6", className)} {...props}>
+			<Card>
+				<CardHeader className="text-center">
+					<CardTitle className="text-xl">Welcome back</CardTitle>
+					<CardDescription>
+						Login with your Apple or Google account
+					</CardDescription>
+				</CardHeader>
+				<CardContent>
+					<form>
+						<div className="grid gap-6">
+							<div className="grid gap-6">
+								<div className="grid gap-2">
+									<Label htmlFor="email">Email</Label>
+									<Input
+										id="email"
+										type="email"
+										placeholder="m@example.com"
+										required
+									/>
+								</div>
+								<div className="grid gap-2">
+									<div className="flex items-center">
+										<Label htmlFor="password">Password</Label>
+										<a
+											href="#"
+											className="ml-auto text-sm underline-offset-4 hover:underline"
+										>
+											Forgot your password?
+										</a>
+									</div>
+									<Input id="password" type="password" required />
+								</div>
+								<Button type="submit" className="w-full">
+									Login
+								</Button>
+							</div>
+							<div className="text-center text-sm">
+								Contact support if you have any login issues
+							</div>
+						</div>
+					</form>
+				</CardContent>
+			</Card>
+			<div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 [&_a]:hover:text-primary  ">
+				Microfinance management system. &copy; {currentYear} All rights
+				reserved. <br />
+				Developed by <a href="https://farid-alam.com/">Farid Alam</a>
+			</div>
+		</div>
+	);
+}
