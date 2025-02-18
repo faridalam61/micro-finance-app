a6f66b816e3a6ac9fd4c71ac78d4ac844d07a51b
Farid Alam <faridalam62@gmail.com>
2025-02-19 00:00:48 +0600

    Initiated auth services

---
+++ backend/src/modules/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { TLogin } from "./auth.interface";
+
+// login user
+const login = async (payload: TLogin) => {
+	// check if user is exist
+	// check if user is blocked
+	// check id password
+	// generate access token
+	// Generate refress token
+	// return access token and refress token
+};
+
+// change password
+const changepassword = async () => {};
+
+// refresh token
+const refreshToken = async () => {};
+
+// logout user
+const logout = async () => {};
+// get user info
+const getUser = async () => {};
