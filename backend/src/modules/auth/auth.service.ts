1c5a0614f64005d8c4a8df6d19ccaa469ca53af4
Farid Alam <faridalam62@gmail.com>
2025-02-21 00:10:17 +0600

    updated auth services

--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { getUserByPhone } from "../../utils/findUser";
-import { TLogin } from "./auth.interface";
+import { getUserById, getUserByPhone } from "../../utils/findUser";
+import { User } from "../user/user.model";
+import { TChangePassword, TLogin } from "./auth.interface";
 import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+import jwt, { JwtPayload } from "jsonwebtoken";
 
 // login user
 const login = async (payload: TLogin) => {
@@ -44,8 +45,47 @@ const login = async (payload: TLogin) => {
 	return { accessToken, refreshToken };
 };
 
-// change password
-const changepassword = async () => {};
+// change password by admin
+const changePasswordByAdmin = async (id: string, password: string) => {
+	const hashedPassword = await bcrypt.hash(password, 10);
+	await User.findByIdAndUpdate(id, { password: hashedPassword });
+};
+
+// change password by user
+const changePasswordByUser = async (
+	userData: JwtPayload,
+	payload: TChangePassword
+) => {
+	// check if user is exist
+	const user = await getUserById(userData.id);
+	if (!user) {
+		throw new Error("User not found");
+	}
+
+	// Check if old password is matched
+	const matchPassword = await bcrypt.compare(
+		payload.oldPassword,
+		user.password
+	);
+
+	if (!matchPassword) {
+		throw new Error("Password didnt match");
+	}
+	const checkSamePassword = await bcrypt.compare(
+		payload.newPassword,
+		user.password
+	);
+
+	// check new password and old password is not same
+	if (checkSamePassword) {
+		throw new Error("Old password and new password cant be same");
+	}
+	// hash password
+	const hashedPassword = await bcrypt.hash(payload.newPassword, 10);
+
+	// update database
+	await User.findByIdAndUpdate(userData.id, { password: hashedPassword });
+};
 
 // refresh token
 const refreshToken = async () => {};
@@ -53,4 +93,16 @@ const refreshToken = async () => {};
 // logout user
 const logout = async () => {};
 // get user info
-const getUser = async () => {};
+const getUser = async (userId: string) => {
+	const user = await User.findById(userId);
+	return user;
+};
+
+export const authServices = {
+	login,
+	logout,
+	getUser,
+	changePasswordByAdmin,
+	changePasswordByUser,
+	refreshToken,
+};
